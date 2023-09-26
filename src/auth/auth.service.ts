7d04de2ec8fb0f035dd227697dbd2a50cdf7d1bb
Peter <phuchoang.inf@gmail.com>
2023-09-26 11:00:55 +0700

    Add jwt and guard flow

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
-    return {
-      msg: 'Sign up here',
-    };
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+  async signup(dto: AuthDto) {
+    // Gen the password hash
+    const hash = await argon.hash(dto.password);
+    // Save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+        throw error;
+      }
+    }
   }
 
-  signin() {
-    return 'Sign in here';
+  async signin(dto: AuthDto) {
+    // Find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if not found, throw error
+    if (!user) {
+      throw new ForbiddenException('Credentials not valid');
+    }
+
+    // Compare the password hash with the password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if not match, throw error
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials not valid');
+    }
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const access_token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return { access_token };
   }
 }
